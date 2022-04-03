f18d9340428a65fd3b542cc8d5080c57f8733857
Raymond Bernardo <rbernardo@stemsims.com>
2022-04-03 13:25:58 -0400

    fix

--- src/lib/inc/NetworkReceive.hpp
+++ src/lib/inc/NetworkReceive.hpp
@@ -2,9 +2,9 @@
 
 class NetworkReceive {
     public:
-        uint32_t can_id;
-        uint16_t instance_id;
-        uint16_t size;
+        u_int32_t can_id;
+        u_int16_t instance_id;
+        u_int16_t size;
         u_int8_t *data;
 
         void fromByteArray(u_int8_t *buff);
