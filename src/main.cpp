6423b0f1320476d9fc29ccbc22b1733a154c588c
Raymond Bernardo <rbernardo@stemsims.com>
2022-04-07 21:50:46 -0400

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <map>
 #include "lib/inc/DataLinkRecive.hpp"
 #include "lib/inc/PythonScripts.hpp"
 #include "lib/inc/NetworkReceive.hpp"
@@ -10,6 +11,12 @@ int main(int argc, char *argv[]) {
     DataLinkReceive dataLink;
     dataLink.init();
 
+    // std::map<int, > modules;
+
+    // //Datamodule registration
+    // modules.insert(std::make_pair(module->can_id_, module));
+    // modules.insert(std::make_pair(module->can_id_, module));
+
     int timeout = 0;
 
     while (timeout < 30) {
@@ -17,14 +24,16 @@ int main(int argc, char *argv[]) {
         if (dataLink.read(data)) {
             NetworkReceive network;
             network.fromByteArray(dataLink.buffer);
-            printf("CAN ID: %08x\n", network.can_id);
-            printf("INSTANCE ID: %02x\n", network.instance_id);
-            printf("SIZE: %02x\n", network.size);
-            printf("DATA: ");
-            for (int i = 0; i < network.size; i++) {
-                printf("%02x ", network.data[i]);
-            }
-            printf("\n");
+            // printf("CAN ID: %08x\n", network.can_id);
+            // printf("INSTANCE ID: %02x\n", network.instance_id);
+            // printf("SIZE: %02x\n", network.size);
+            // printf("DATA: ");
+            // for (int i = 0; i < network.size; i++) {
+            //     printf("%02x ", network.data[i]);
+            // }
+            // printf("\n");
+
+
 
             dataLink.flush();
         }
