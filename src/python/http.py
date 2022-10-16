9ba33a347e48b381535a24f0d365fc4b7a2166fe
Raymond Bernardo <rbernardo@stemsims.com>
2022-10-15 17:48:01 -0700

    Remove the http file since it conflicts with requests

--- src/python/http.py
+++
@@ -1,9 +0,0 @@
-# import requests
-
-def runs(p1, p2):
-    # requests.post(
-    #     "http://localhost:9000/api/" + path,
-    #     json=body
-    # )
-    # print(path, body)
-    print(p1, p2)
