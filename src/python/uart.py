8e3446ab6a0de191eceec485d87d789c21191844
Raymond Bernardo <rbernardo@stemsims.com>
2023-02-05 10:31:29 -0800

    catch any UDP errors for external service
    - We shouldn't error if the external service is down since it's optional

--- src/python/uart.py
+++ src/python/uart.py
@@ -4,15 +4,22 @@ ser = serial.Serial(os.environ.get("UART_PORT"), os.environ.get("UART_BAUD_RATE"
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 
-UDP_PORT = 8000
+INTERNAL_UDP_PORT = os.environ.get("INTERNAL_UDP_PORT")
+INTERNAL_UDP_HOST = os.environ.get("INTERNAL_UDP_HOST")
+EXTERNAL_UDP_PORT = os.environ.get("EXTERNAL_UDP_PORT")
+EXTERNAL_UDP_HOST = os.environ.get("EXTERNAL_UDP_HOST")
 
 def runs(path, body):
     data = json.dumps({
         "model": path,
         **body,
     }).encode()
-    sock.sendto(data, ("127.0.0.1", UDP_PORT))
-    sock.sendto(data, ('api.ufsolargators.org', UDP_PORT))
+    sock.sendto(data, (INTERNAL_UDP_PORT, INTERNAL_UDP_PORT))
+    # Try to send to external server, but if it fails then ignore it
+    try:
+        sock.sendto(data, (EXTERNAL_UDP_PORT, EXTERNAL_UDP_HOST))
+    except socket.error:
+        pass
 
 def wait_for_transmission():
     global ser
