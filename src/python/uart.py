a920913fc60177ebc2cab9f6e000403c1856fa43
Raymond Bernardo <rbernardo@stemsims.com>
2023-02-05 00:32:21 -0800

    replace POST requests with UDP

--- src/python/uart.py
+++ src/python/uart.py
@@ -1,12 +1,18 @@
-import serial, requests, os
+import serial, os, socket, json
 
 ser = serial.Serial(os.environ.get("UART_PORT"), os.environ.get("UART_BAUD_RATE"))
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+UDP_PORT = 8000
 
 def runs(path, body):
-    requests.post(
-        f"{os.environ.get('GUI_SERVICE')}/api/{path}",
-        json=body
-    )
+    data = json.dumps({
+        "model": path,
+        **body,
+    }).encode()
+    sock.sendto(data, ("127.0.0.1", UDP_PORT))
+    sock.sendto(data, ('api.ufsolargators.org', UDP_PORT))
 
 def wait_for_transmission():
     global ser
